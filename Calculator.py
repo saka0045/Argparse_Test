cbc66f96516a3718845f88767866f37add6a9191
Yuta  Sakai;Sakai.Yuta@mayo.edu;14571650 <m006703@dlmpcim01.mayo.edu>
2017-05-26 14:22:48 -0500

    Calculator improvements

--- Calculator.py
+++ Calculator.py
@@ -23,16 +23,34 @@ parser.add_argument('--subtract',
                     action='store_true',
                     help="Subtracts the second integer from the first integer")
 
+parser.add_argument('--multiply',
+                    required=False,
+                    action='store_true',
+                    help="Multiply the first and second integers")
+
+parser.add_argument('-v', '--verbose',
+                    required=False,
+                    default=False,
+                    action="store_true",
+                    help="Displays function and result")
+
 args = parser.parse_args()
 
 if args.add:
     addint = args.firstint + args.secondint
     print(addint)
+    if args.verbose:
+        print(str(args.firstint) + ' + ' + str(args.secondint) + ' = ' + str(addint))
     sys.exit(1)
 
 if args.subtract:
     subtractint = args.firstint - args.secondint
     print(subtractint)
+    sys.exit(1)
+
+if args.multiply:
+    multiplyint = args.firstint * args.secondint
+    print(multiplyint)
     sys.exit(1)
 
 print('What do you want to do with these numbers??')
